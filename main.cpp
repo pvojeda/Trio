c41a6e8efaf1e9613ae4ad2c3bb0bcbf1deeb77c
Pvojeda <pojeda@mail.csuchico.edu>
2018-04-16 17:58:08 -0700

    arrangingrgb

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
   //DO NOT CHANGE WITHIN THIS AREA...
@@ -8,7 +7,18 @@ int main()
   cout<<"Enter Red, Green, and Blue values: ";
   cin>>red>>green>>blue;
   //...END OF "DO NOT CHANGE" AREA
-
+if( blue>red)
+{
+swap (blue,red);
+}
+if( blue > green);
+{
+swap (blue,green);
+}
+if ( green> red)
+{
+swap (green,red);
+}
 
 
 
@@ -18,4 +28,37 @@ int main()
   cout<<"RGB: "<<red<<","<<green<<","<<blue<<endl;
   return 0;
   //...END OF "DO NOT CHANGE" AREA
+}
+void rearrangeColor(int&Red, int&Green ,int&Blue)
+{
+int tempRed, tempGreen ,tempBlue;
+
+if ( Green>Red)
+{
+    Red = tempGreen;
+}
+if ( Blue>Red)
+{
+    Red = tempBlue;
+}
+if ( Green = Red)
+{
+Green=0;
+}
+if((Green < tempBlue)&&(Red !=tempBlue))
+{
+    Green = tempBlue;
+}
+if((Green < tempRed)&&(Red !=tempRed))
+{
+    Green = tempRed;
+}
+if(Blue > tempRed)
+{
+    Blue = tempRed;
+}
+if(Blue > tempGreen)
+{
+    Blue = tempGreen;
+}
 }
